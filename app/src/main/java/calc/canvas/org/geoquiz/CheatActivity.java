80a487f7952603f367ef5068464bc662d618679a
admiralsilver <krishna.c.kandula@gmail.com>
2016-03-06 10:11:02 -0600

    1.07 - Finished functionality for cheat button. Completed cheat_activity

--- app/src/main/java/calc/canvas/org/geoquiz/CheatActivity.java
+++ app/src/main/java/calc/canvas/org/geoquiz/CheatActivity.java
@@ -1,13 +1,47 @@
 package calc.canvas.org.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String EXTRA_ANSWER_IS_TRUE = "calc.canvas.org.geoquiz.answer_is_true";
+    public boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, mAnswerIsTrue);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
